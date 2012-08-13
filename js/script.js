235d564617af3c2f58ed004a4252dc5168d0b784
Felipe Velasquez Castro <Felipe.velasquezc@gmail.com>
2012-08-13 17:39:19 -0400

    sd

--- js/script.js
+++ js/script.js
@@ -2,11 +2,31 @@
 
 */
 
+var bloque;
+
 $(function(){
 
 $(".btnAtras").on("click",function(){
-$(".ui-overlay").hide();
+	$(".ui-overlay").hide();
 	$("#frmReserva_content").hide();
+})
+
+$(".btnAccion").on("click",function(){
+	$(".ui-overlay").hide();
+	$("#frmReserva_content").hide();
+	$(bloque).data({
+		nombre:$("#txtNombreSolicitante").val(),
+		email:$("#txtEmail").val(),
+		telefono:$("#txtTelefono").val(),
+		notas:$("#tarNotas").val()
+	}).children().removeClass("bloque-ocupado")
+})
+
+$(".bloque").on("click",function(){
+	$(".ui-overlay").show();
+	$("#frmReserva_content").show();
+	bloque = $(this)
+
 })
 
 });
