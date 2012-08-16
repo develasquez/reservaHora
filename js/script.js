782081ee534aa8372f602997a7698a41e25f513c
Felipe Velasquez Castro <Felipe.velasquezc@gmail.com>
2012-08-16 09:29:15 -0400

    asd

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ $(".btnAtras").on("click",function(){
 			$("#txthora").val($($("#horas1 div")[idHora-1]).text().trim());
 			 
 	var data = $(bloque).data(); 
-		$("#txtNombreSolicitante").val()= data.nombre );
+		$("#txtNombreSolicitante").val(data.nombre );
 		$("#txtEmail").val( data.email);
 		$("#txtTelefono").val( data.telefono);
 		$("#txtEmpresa").val( data.empresa);
